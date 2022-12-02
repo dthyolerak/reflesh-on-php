a4adfca4c42d52b6a131de7f015b78ad1594aabb
Donnex Thyolera Kamsonga <Donnex Thyolera Kamsonga>
2022-12-02 12:33:04 +0200

    working on blade layout, yield, extends, section ...

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,21 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello there</title>
-</head>
-<body>
-    <h1>Reflesh on Laravel</h1>
-    @if($array)
-    @foreach ($array as $item)
-       <p> {{$item}}</p>
-    @endforeach
-    @endif
-    <p>hello</p>
-    @for ($i = 0; $i < 3; $i++)
-        <p>the vaule of i is {{$i}} </p>
-    @endfor
-</body>
-</html>
+@extends('layout.layout')
+@section('page-title')
+    hello - Bakeryhouse
+@endsection
+
+@section('content')
+<h1>Reflesh on Laravel</h1>
+@if($array)
+@foreach ($array as $item)
+   <p> {{$item}}</p>
+@endforeach
+@endif
+<p>hello</p>
+@for ($i = 0; $i < 3; $i++)
+    <p>the vaule of i is {{$i}} </p>
+@endfor
+@endsection
