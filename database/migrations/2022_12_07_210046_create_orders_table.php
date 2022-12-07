7010627476f06057550c65c539beb2d6065ded73
Donnex Thyolera Kamsonga <Donnex Thyolera Kamsonga>
2022-12-07 23:10:39 +0200

    running migration, to create tables and migrate tabls

---
+++ database/migrations/2022_12_07_210046_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('full_name');
+            $table->string('email')->uniqid();
+            $table->string('status');
+            $table->string('food_name');
+            $table->integer('amount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
