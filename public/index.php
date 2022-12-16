45dc88f24faf81332b085217ae773e9053c3fd5f
Donnex Thyolera Kamsonga <Donnex Thyolera Kamsonga>
2022-12-16 09:37:30 +0200

    ramsey/uuid has been add

--- public/index.php
+++ public/index.php
@@ -1,11 +1,18 @@
 <?php
 
- require  "../app/PaymentGateway/Gospel/Transcation.php";
- require  "../app/PaymentGateway/Gospel/Customer.php";
- require  "../app/PaymentGateway/Music/Transcation.php";
- require  "../app/Notification\Email.php";
+//  require  "../app/PaymentGateway/Gospel/Transcation.php";
+//  require  "../app/PaymentGateway/Gospel/Customer.php";
+//  require  "../app/PaymentGateway/Music/Transcation.php";
+//  require  "../app/Notification\Email.php";
+ spl_autoload_register(function($class){
+   $path = __DIR__ ."/../". lcfirst(str_replace("\\",'/', $class).".php");
+    if(file_exists($path)){
+        require $path;
+    }
+ });
+ 
 use  App\PaymentGateway\Gospel\Transcation as GospelTranscation;
-use App\PaymentGateway\Muisc\Transcation as MusicTranscation;
+use App\PaymentGateway\Music\Transcation as MusicTranscation;
 
 
  var_dump(new GospelTranscation());
